c30cff6605da08d34d7bb7a7ce917d06a1a812ed
Foat Akhmadeev <foat@fmga.ru>
2023-09-04 10:09:20 +0300

    remote copy/paste

--- init.lua
+++ init.lua
@@ -321,7 +321,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'python', 'tsx', 'typescript', 'vimdoc', 'vim', 'java', 'kotlin' },
+  ensure_installed = { 'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'java', 'kotlin' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
