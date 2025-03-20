39be5160741933a200b8df0b6a62bd1db0c54feb
Foat Akhmadeev <foat@fmga.ru>
2025-03-20 10:27:55 +0300

    mac spectre fix

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -4,4 +4,20 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
   },
+  config = function()
+    if vim.loop.os_uname().sysname == 'Darwin' then
+      require("spectre").setup({
+        replace_engine = {
+          ["sed"] = {
+            cmd = "sed",
+            args = {
+              "-i",
+              "",
+              "-E",
+            },
+          },
+        },
+      })
+    end
+  end
 }
